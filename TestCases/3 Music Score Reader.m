6711d6c5f663d561a3ade65c178389280e8fe4de
BeshoyHani <be4oy.hani@gmail.com>
2021-12-22 20:52:03 +0200

    Initial Commit

---
+++ TestCases/3 Music Score Reader.m
@@ -0,0 +1,36 @@
+img = imread('TwinkleTwinkleLittleStar.bmp');
+
+%Extracting Stafflines 
+dilation_SE = strel('line', 10, 0);
+dilated_img = imdilate(img, dilation_SE);
+%figure, imshow(dilated_img);
+
+%Subtract Stafflines from image
+img2 = dilated_img - img;
+img2 = imcomplement(img2);
+figure, imshow(img2);
+
+%Extracting Stafflines 
+dilation_SE2 = strel('line', 3, 90);
+dilated_img2 = imclose(img2, dilation_SE2);
+dilated_img2 = imfill(dilated_img2);
+figure, imshow(dilated_img2);
+
+%Shapeining image using laplacial filter
+% Laplacian=[-1 -1 -1; -1 9 -1; -1 -1 -1];
+% img = rgb2gray(dilated_img2);
+% laplacian_img=uint8(conv2(img, Laplacian, 'same'));
+% figure, imshow(laplacian_img);
+
+% erosion_SE = strel('line', 2, 90);
+% eroded_img = imerode(dilated_img, erosion_SE);
+% figure, imshow(eroded_img);
+
+% opened_SE = strel('line', 10, 90);
+% opened_img = imopen(dilated_img, opened_SE);
+% figure, imshow(opened_img);
+
+
+% eroded_img = imfill(eroded_img);
+%figure, imshow(dilated_img);
+%figure, imshow(img);
