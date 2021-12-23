3b1c810b5fd842092fb2b828dd83322054f74ad0
BeshoyHani <be4oy.hani@gmail.com>
2021-12-23 15:17:18 +0200

    Fixing previous Fixes for Some Stuff

--- TestCases/Music_Score_Reader.m
+++ TestCases/Music_Score_Reader.m
@@ -1,28 +1,26 @@
 img = imread('TwinkleTwinkleLittleStar.bmp');
-img = rgb2gray(img);
-BW = edge(img,'Sobel',[],'horizontal');
-figure, imshow(BW);
-
+img = imcomplement(img);
 
 %Extracting Stafflines 
-%dilation_SE = strel('line', 10, 0);
-%dilated_img = imdilate(img, dilation_SE);
+dilation_SE = strel('line', 10, 0);
+dilated_img = imopen(img, dilation_SE);
 %figure, imshow(dilated_img);
 
 %Subtract Stafflines from image
-%img2 = dilated_img - img;
-%img2 = imcomplement(img2);
-%figure, imshow(img2);
+img2 = img - dilated_img;
+figure, imshow(img2);
 
-%Extracting Stafflines 
-%dilation_SE2 = strel('line', 3, 90);
-%dilated_img2 = imclose(img2, dilation_SE2);
-%dilated_img2 = imfill(dilated_img2);
-%figure, imshow(dilated_img2);
+%Extracting Stafflines
+eroded_SE = strel('line', 2, 90);
+img2 = imopen(img2, eroded_SE);
+%im = max(img2, [], 3);
+%img2 = imfill(im>90,'holes');
+figure, imshow(img2);
 
 %Shapeining image using laplacial filter
 % Laplacian=[-1 -1 -1; -1 9 -1; -1 -1 -1];
-% img = rgb2gray(dilated_img2);
+% Mean = [1 1 1; 1 1 1; 1 1 1];
+% img = rgb2gray(img2);
 % laplacian_img=uint8(conv2(img, Laplacian, 'same'));
 % figure, imshow(laplacian_img);
 
